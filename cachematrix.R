dd6d52b16966e7563bf127c0cb3c5252b4b18473
vidyaramesh28 <vidyavasista@gmail.com>
2016-03-20 15:01:33 -0400

    Coursera_R Programming_Programming assignment 2
    Create a cacheable matrix, find its inverse and display on console.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a matrix object capable of storing its inverse. 
+## cacheSolve retrieves this matrix from the cache, applies solve() on it to find its inverse and prints it out to the console.
 
-## Write a short comment describing this function
+## creates a cache-able matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(solve) m <<- solve
+	getinverse <- function() m
+	list(set = set, get = get,
+		setinverse = setinverse,
+	getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## finds inverse of cached matrix using solve().
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	m <- x$getinverse()
+	if(!is.null(m)) {
+		message("Getting cached data")
+	return(m)
+	}
+	data <- x$get()
+	m <- solve(data, ...)
+	x$setinverse(m)
+	m
 }
